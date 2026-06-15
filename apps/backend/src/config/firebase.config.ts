e8432d20b2548c819e994f36765998fd07af4818
Karthikkumar N <es6developer@gmail.com>
2026-06-15 16:23:00 +0530

    refactor: use firebaseCredentials file as primary source

--- apps/backend/src/config/firebase.config.ts
+++ apps/backend/src/config/firebase.config.ts
@@ -2,9 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { firebaseCredentials } from './firebase.credentials';
 
 export default registerAs('firebase', () => ({
-  projectId: process.env.FIREBASE_PROJECT_ID || firebaseCredentials.project_id,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL || firebaseCredentials.client_email,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    : firebaseCredentials.private_key,
+  projectId: firebaseCredentials.project_id,
+  clientEmail: firebaseCredentials.client_email,
+  privateKey: firebaseCredentials.private_key,
 }));
