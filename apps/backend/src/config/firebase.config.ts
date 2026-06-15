3aaaffaf347908096553d5ed169af213e3b5a445
Karthikkumar N <es6developer@gmail.com>
2026-06-15 16:22:05 +0530

    feat: add Firebase credentials constants file as fallback

--- apps/backend/src/config/firebase.config.ts
+++ apps/backend/src/config/firebase.config.ts
@@ -1,9 +1,10 @@
 import { registerAs } from '@nestjs/config';
+import { firebaseCredentials } from './firebase.credentials';
 
 export default registerAs('firebase', () => ({
-  projectId: process.env.FIREBASE_PROJECT_ID || '',
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL || '',
+  projectId: process.env.FIREBASE_PROJECT_ID || firebaseCredentials.project_id,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL || firebaseCredentials.client_email,
   privateKey: process.env.FIREBASE_PRIVATE_KEY
     ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    : '',
+    : firebaseCredentials.private_key,
 }));
---
+++ apps/backend/src/config/firebase.credentials.ts
@@ -0,0 +1,42 @@
+export const firebaseCredentials = {
+  type: 'service_account',
+  project_id: 'dabbu-c8e78',
+  private_key_id: '11aea8df8e5937a37c7cdec4ef2630a7a6676642',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\n' +
+    'MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCmZX/EC+lPfLvO\n' +
+    'rRYC2uzxBOruZyAzj4hACrshLS0fIzRCUgVCvIML6KKaRN87w2Q+TluysB1eerjW\n' +
+    'EXPRBi9Iqmf85j8C4Bb6f3yE6EtBv4bcbSRCjEAbrZChMJaUY/TKS96d4xYWvVGT\n' +
+    'f6u/Isw+Wxq8ahZOsN0BsPzljJ4i63CtDvtIfVg3OyBrRaKnOGYDuFLy+jvOewrj\n' +
+    '0PO19NewP1Es96/tZyAMTf76dpvK6vMy76zO2Gy8InzJVk8dN26IaFRQsUHO6XI4\n' +
+    'kL3FlMcHlYn2vd0YsAzVt4KTONAaeffhI4xqQIsbdeqDPcMLjr4s+2BrF5bR2uNU\n' +
+    'nYN2SYNfAgMBAAECggEARLniahpTXJHTzjrePwKmX6wczqgIfFNZvr3P6ZWLr/Mt\n' +
+    '6ACRjK1o4PfOhTJSE5CR7EwYArdMAhRlGIGv2NMZn4VGmwWMXAcrZn5Wc1ywjLDr\n' +
+    'Jkrr8ZKU/fmTpYf4gcTBO/bXNVYwbub19RqD2NeqDU06vzyABzN0rPipk7QfMqNq\n' +
+    'Io8NqDbXyDRkxd0B0udN7cxO+eYvOao17ooJOmpQt477KKZYIBu9NtjpTYcGEF85\n' +
+    'zTyLIXKmvdBfZz7nYmogGlyYBvyrkGrJVLP4UgzpRoThYiQli5mbxMse7XD1S4Rl\n' +
+    'I+Va5kvxYw/JmwTMvbogWowevp8wprWN/TvAE0xokQKBgQDbNoLy4M8YRus/gk3/\n' +
+    '7+0aTMKb12NGizQ5eAF12B84ARu1pbducYQ/FFpHyTMwzlfT4XR6cpztF5cdPhMe\n' +
+    'szZ8yucNqiZPSCKjzTG0/qxSupPQUZlY2DtwB70l8V/HVUhq+nz7cSBB7dv/szaA\n' +
+    'oqd7gsoKRhbBs7sKjk3DqOeczwKBgQDCUffoWd41blYLiFGQqZk1R9KY1k13rGu9\n' +
+    'Oh8FmdIOmk1oVqO1SCeyp4inyWmN6zjhz9bnCnquqmvSLB84l3+z7R3pdKl/0gzW\n' +
+    'MFgdrFDsYOKJCcK0nD0tvwp5rihk//v2eN926CkvASY+8FTQJh2nhcWt3XyBZqR4\n' +
+    '02JdBej0cQKBgCli2EQsh0zJDt71pe49COGfPlqXxolp/Jkg+/2YqQQ0yVt5BvGW\n' +
+    'Aua52/WTmdOMEW7SMOBxRQlKG+CMeKWiJhZXZFnjFsp/DvMn6UuHjVctHH0G+tGY\n' +
+    'OViETYSdVnzWsA5qbjA2gmWQF6nl1tNgYghif+yCoQc+/IAYr7vR5rBnAoGAOcPY\n' +
+    'KFAeyE9mcMMzTAe+raIOLrT2Dm9JD9VpR2b0lLa18CBhRhCPDILVEtvC7xdPr4Dz\n' +
+    'Z3csBSRQLQhEt7QBmGESlUXC5WZxvnJcn2WMINikPwyRvtL7NNAnswhcpbmbEyfX\n' +
+    'dieJppl23xree4n5IsWXgBnpWyZCGlDsWq7ZE2ECgYAQttCwaw6FEzd6u+dEmB0r\n' +
+    'Ptr42iSgk4mHb+XacuBtENBf2GnSUV2Pqqho16mtunpYt2CIhKVo9+tbRuoCU7+9\n' +
+    '7V4xQCTqrYApwGPHnVb+1bT1OxLiaalyG0s221Dbgs5ZsDfJn8lXkom9f2qjiKqS\n' +
+    'LuAa+veTlxx0wEMvb9tHiw==\n' +
+    '-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-fbsvc@dabbu-c8e78.iam.gserviceaccount.com',
+  client_id: '115972771486394855294',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40dabbu-c8e78.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com',
+};
